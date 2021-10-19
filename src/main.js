8a1f5d974b7b99725f80e34e1dc96eac972f1c83
connectshark <bobhus394@gmail.com>
2021-10-19 16:34:30 +0800

    改用cli

--- src/main.js
+++ src/main.js
@@ -2,18 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import { createI18n } from 'vue-i18n'
+import i18n from './i18n'
 import './assets/css/reset.css'
 import './assets/font/twicon.css'
-
-import messages from '@intlify/vite-plugin-vue-i18n/messages'
-
-const i18n = createI18n({
-  legacy: false,
-  globalInjection: true,
-  locale: 'tw',
-  messages
-})
 
 createApp(App)
   .use(router)
