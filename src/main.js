1d93ac23cfdced4945104285b9d287b04a40b328
connectshark <bobhus394@gmail.com>
2021-10-19 14:05:39 +0800

    移動字型位置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import './assets/css/reset.css'
+import './assets/font/twicon.css'
 
 createApp(App)
   .use(router)
